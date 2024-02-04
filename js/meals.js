c7d4b7c5d291b8b29dc47c4a6b2580151c333079
sabbir1452 <sabbirrahman1452@gmail.com>
2024-02-05 05:55:38 +0600

    ;lajdl;

--- js/meals.js
+++ js/meals.js
@@ -9,12 +9,11 @@ const displayMeals = (meals) => {
   const mealsContainer = document.getElementById("meal-container");
   mealsContainer.innerHTML = ``;
   meals.forEach((meal) => {
-    console.log(meal);
+    // console.log(meal);
     const mealDiv = document.createElement("div");
     mealDiv.classList.add("col");
     mealDiv.innerHTML = `
-    <div class="col">
-            <div class="card">
+            <div onclick='loadMealDetail(${meal.idMeal})' class="card">
               <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -36,4 +35,32 @@ const searchFood = () => {
   loadMeals(searchText);
   searchField.value = "";
 };
-// loadMeals("a");
+
+const loadMealDetail = (idMeal) => {
+  //   console.log("get detail of id", idMeal);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+  //   console.log(url);
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+const displayMealDetails = (meal) => {
+  const detailContainer = document.getElementById("detail-container");
+  detailContainer.innerHTML = ``;
+  const mealDiv = document.createElement("div");
+  mealDiv.classList.add("card");
+  mealDiv.innerHTML = `
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+   <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">
+        Some quick example text to build on the card title and make up
+        he bulk of the card's content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  `;
+  detailContainer.appendChild(mealDiv);
+};
+loadMeals("");
